eea7486d0ce91e5de2cd350852c709b508d2103d
mikerw <mikerw@live.unc.edu>
2016-07-12 13:04:59 -0400

    made questions.sh work!

---
+++ questions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 5 questions
+echo Hello, what is your age?
+read ans1
+
+echo What is the furthest distance you have travelled in km?
+read ans2
+
+echo What year did you graduate from highschool?
+read ans3
+
+echo Do you commute to campus?
+read ans4
+
+echo Have you learned anything in INLS161?
+read ans5
+
+DATE=`date`
+
+ANSWERS="$ans1,$ans2,$ans3,$ans4,$ans5,$DATE"
+echo $ANSWERS >> eureka.csv
